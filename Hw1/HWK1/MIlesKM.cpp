9a85d740ec15e1e48ebcf6a9362ad53d77688020
Daniel Kegley <dckegley@alaska.edu>
2021-01-29 12:30:45 -0900

    Finished MilesKm.cpp

---
+++ Hw1/HWK1/MIlesKM.cpp
@@ -0,0 +1,16 @@
+//MilesKM.cpp
+//Daniel Kegley
+//29 January 2021
+//Converts Miles to KM
+
+#include <iostream>
+
+int mikmmain()
+{
+	int mi;
+	std::cout << "Miles to Kilometers converter" << std::endl;
+	std::cout << "How many miles? ";
+	std::cin >> mi;
+	std::cout << mi * 1.609;
+	return 0;
+}
